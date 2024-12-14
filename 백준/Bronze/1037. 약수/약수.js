b1571b02481d03c6a6b77bee013cccfbbf8c192c
윤진 <151013932+maruru301@users.noreply.github.com>
2024-12-15 02:49:37 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.js
+++ 백준/Bronze/1037. 약수/약수.js
@@ -2,16 +2,7 @@ const fs = require('fs');
 const [n, input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const inputArr = input.trim().split(' ').map(Number);
 
-let max = inputArr[0];
-let min = inputArr[0];
-
-for (let i = 0; i < n; i++) {
-    if (inputArr[i] > max) {
-        max = inputArr[i];
-    }
-    if (inputArr[i] < min) {
-        min = inputArr[i];
-    }
-}
+const max = Math.max(...inputArr);
+const min = Math.min(...inputArr);
 
 console.log(max * min);
