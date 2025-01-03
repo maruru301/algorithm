567db862ac1826fa925a96e9388db92cc67d8c10
윤진 <151013932+maruru301@users.noreply.github.com>
2025-01-04 01:24:34 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.21 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,7 +1,7 @@
 function solution(intStrs, k, s, l) {
     let result = [];
     
-    for (arr of intStrs)
+    for (let arr of intStrs)
         result.push(Number(arr.slice(s, s + l)));
     
     result = result.filter(x => x > k)
