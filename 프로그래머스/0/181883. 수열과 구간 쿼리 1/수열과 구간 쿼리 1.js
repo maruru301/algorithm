82fa875a843a6502c4e4b12d0f429ff3c2e108ac
윤진 <151013932+maruru301@users.noreply.github.com>
2025-01-09 20:25:43 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 3.80 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,11 +1,7 @@
 function solution(arr, queries) {
-    let result = [];
-    
     queries.map(([s, e]) => {
-        for(let i = 0; i < arr.length; i++) {
-            if(s <= i && i <= e)
-                arr[i]++;
-        }
+        for(let i = s; i <= e; i++)
+            arr[i]++;
     })
     
     return arr;
