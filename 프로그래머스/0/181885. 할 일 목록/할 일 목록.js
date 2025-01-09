12d7f90d559e3edc8215776539a845ba68c1e848
윤진 <151013932+maruru301@users.noreply.github.com>
2025-01-09 20:07:35 +0900

    [level 0] Title: 할 일 목록, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,16 +1,11 @@
 function solution(todo_list, finished) {
     let result = [];
-    let falseList = [];
     
     for(let i = 0; i < finished.length; i++) {
         if(finished[i] === false) {
-            falseList.push(i);
+            result.push(todo_list[i]);
         }
     }
-    
-    for(let idx of falseList) {
-        result.push(todo_list[idx]);
-    }
-    
+
     return result;
 }
