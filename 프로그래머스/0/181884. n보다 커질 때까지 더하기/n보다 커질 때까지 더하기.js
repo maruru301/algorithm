0cad6380560825fbe1191a5062b09a359b2520cb
윤진 <151013932+maruru301@users.noreply.github.com>
2025-01-09 20:12:07 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers, n) {
+    let result = 0;
+    
+    for(let num of numbers) {
+        result += num;
+        if(result > n) {
+            return result
+        }
+    }
+}
