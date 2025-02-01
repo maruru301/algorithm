9373b8e605c91beaed4c923d2aadade5ed64cb5f
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-02 00:12:22 +0900

    [level 0] Title: 1로 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,12 +1,12 @@
 function solution(num_list) {
-    return num_list.reduce((a,c)=>a+one(c),0);
-
-    function one(num) {
-        let count = 0;
-        while(num > 1){
-            num%2===0? num = num/2: num = (num-1)/2;
-            count++
+    let result = 0;
+    
+    num_list.forEach(num => {
+        while(num !== 1) {
+            num = (num % 2 === 0) ? num / 2 : (num - 1) / 2;
+            result++;
         }
-        return count;
-    }
+    });
+    
+    return result;
 }
