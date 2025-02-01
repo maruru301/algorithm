57b56e9a7aebd2bed0779648ffc29efd73a6186a
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-02 00:30:27 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,16 +1,14 @@
 function solution(num_list) {
-    let add = 0;
-    let mul = 1;
     let result = 0;
     
-    for(let i in num_list){
-        if(num_list.length >= 11){
-            add += num_list[i]
-            result = add
-        }else{
-            mul *= num_list[i]
-            result = mul
-        }
+    if(num_list.length >= 11) {
+        for(let num of num_list)
+            result += num;
+    } else {
+        result = 1;
+        for(let num of num_list)
+            result *= num;
     }
-    return result    
+        
+    return result;
 }
