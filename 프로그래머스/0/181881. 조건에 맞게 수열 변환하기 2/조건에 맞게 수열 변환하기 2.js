3cdbc821994739ed1230a39497a715e69cc77111
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-01 23:37:03 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 206.97 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,11 +1,25 @@
 function solution(arr) {
-    var arrBefore = [...arr];
-    var cnt = 0;
-    while(true){
-        var arr = arrBefore.map((e)=> e >= 50 && e % 2 == 0 ? e / 2 :  e < 50 && e % 2 == 1 ? e * 2 + 1 : e);
-        if(JSON.stringify(arr) == JSON.stringify(arrBefore)) break;
-        arrBefore = arr;
-        cnt++;
+    let x = 0; // 반복 횟수
+    let arr2 = arr;
+    
+    while(true) {
+        const check = arr2.map(i => {
+            if (i >= 50 && i % 2 === 0) 
+                return i / 2;
+            else if (i < 50 && i % 2 != 0)
+                return i * 2 + 1;
+            return i;
+        })
+    
+        const isSame = arr2.every((i, el) => i === check[el]) // 기존 배열과 변환된 배열이 같은지 검사
+        
+        if(isSame) // 같다면 반복 종료
+            break;
+        
+        x += 1; // 반복 횟수 증가
+    
+        arr2 = check // 변환된 배열을 기존 배열로 업데이트
     }
-    return cnt;
+    
+    return x;
 }
