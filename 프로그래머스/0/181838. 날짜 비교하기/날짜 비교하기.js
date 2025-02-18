ec9c68d8c10c9467447895fa3c51b5ab0bbda029
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-19 00:30:06 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -2,7 +2,14 @@ function solution(date1, date2) {
     const [year1, month1, day1] = date1;
     const [year2, month2, day2] = date2;
     
-    if (year1 !== year2) return year1 < year2 ? 1 : 0;
-    if (month1 !== month2) return month1 < month2 ? 1 : 0;
-    return day1 < day2 ? 1 : 0;
+    if (year1 !== year2) {
+        return year1 < year2 ? 1 : 0;
+    }
+    if (month1 !== month2) {
+        return month1 < month2 ? 1 : 0;
+    }
+    if (day1 < day2) {
+        return 1;
+    }
+    return 0;
 }
