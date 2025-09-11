0fb0903df9a0d0620de0a82ffd8b86b4b34c3140
윤진 <151013932+maruru301@users.noreply.github.com>
2025-09-11 13:32:03 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 28.56 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,61 +1,47 @@
 function solution(friends, gifts) {
-    let n = friends.length;
+    const n = friends.length;
 
+    // nameMap에 이름 -> 인덱스 매핑
     const nameMap = new Map();
+    friends.forEach((name, idx) => nameMap.set(name, idx));
     
     // 주고 받은 선물 기록 -> 2차원 배열
-    const giftRecordTable = Array.from({ length: n }).map(() => new Array(n).fill(0));
-    
-    friends.forEach((name, idx) => {
-        nameMap.set(name, idx);
-    })
-    
-    // giftRecordTable에 선물 내역 저장
-    gifts.forEach((giftInfo) => {
-        const [giver, receiver] = giftInfo.split(' ');
+    const giftRecordTable = Array.from({ length: n }, () => new Array(n).fill(0));
+    // giver가 receiver에게 선물을 준 횟수 기록
+    gifts.forEach((gift) => {
+        const [giver, receiver] = gift.split(' ');
         giftRecordTable[nameMap.get(giver)][nameMap.get(receiver)]++;
-    })
-    
-    // 선물 지수
-    const giveInfo = new Array(n).fill(0);
-    const receiveInfo = new Array(n).fill(0);
-    const giftRank = new Array(n).fill(0);
+    });
     
     // 선물 지수 계산
-    for(let i = 0; i < n; i++) {
-        giveInfo[i] += giftRecordTable[i].reduce((a, b) => a + b, 0); // 보낸 선물
-        
-        for(let j = 0; j < n; j++) {
-            receiveInfo[i] += giftRecordTable[j][i];  // 받은 선물
-        }
-            giftRank[i] = giveInfo[i] - receiveInfo[i]; // 선물 지수: 보낸 선물 - 받은 선물
+    const giftRank = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        const give = giftRecordTable[i].reduce((sum, val) => sum + val, 0);  // 행의 각 열(column) 합
+        const receive = giftRecordTable.reduce((sum, row) => sum + row[i], 0);  // 모든 행(row)에서 i번째 열 합
+        giftRank[i] = give - receive;
     }
     
-    // 다음 달 받을 선물 수
+    // 다음달에 받을 선물 수
     const nextMonthGift = new Array(n).fill(0);
-    
     for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-
+        for(let j = i + 1; j < n; j++) {
             const iToJ = giftRecordTable[i][j];
             const jToI = giftRecordTable[j][i];
-
+            
             if (iToJ > jToI) nextMonthGift[i]++;
             else if (iToJ < jToI) nextMonthGift[j]++;
             else {
                 // 선물 지수 비교
-                if (giftRank[i] > giftRank[j]) nextMonthGift[i]++;
-                else if (giftRank[i] < giftRank[j]) nextMonthGift[j]++;
+                if (giftRank[i] > giftRank[j]) nextMonthGift[i]++
+                else if (giftRank[i] < giftRank[j]) nextMonthGift[j]++
             }
         }
     }
     
     console.log(nameMap);
     console.log(giftRecordTable);
-    console.log(giveInfo);
-    console.log(receiveInfo);
     console.log(giftRank);
     console.log(nextMonthGift);
     
-    return Math.max(...nextMonthGift)
+    return Math.max(...nextMonthGift);
 }
