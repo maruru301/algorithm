d8a09645166de9f482aadcc469446dce5b47fb6d
윤진 <151013932+maruru301@users.noreply.github.com>
2025-09-15 15:48:23 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,10 @@
+function solution(dot) {
+    let result = 0;
+    
+    const [x, y] = dot;
+    
+    if (x > 0 && y > 0) return 1;
+    else if (x < 0 && y > 0) return 2;
+    else if (x < 0 && y < 0) return 3;
+    else if (x > 0 && y < 0) return 4;
+}
