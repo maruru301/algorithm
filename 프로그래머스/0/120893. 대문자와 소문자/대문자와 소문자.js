b8e35939d5931abe56075030945729541471de5f
윤진 <151013932+maruru301@users.noreply.github.com>
2025-09-25 14:09:24 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -2,8 +2,8 @@ function solution(my_string) {
     let result = '';
     
     result = [...my_string].map(ch => {
-        if (ch.charCodeAt() < 97) return String.fromCharCode(ch.charCodeAt() + 32);
-        else return String.fromCharCode(ch.charCodeAt() - 32)
+        if (ch.charCodeAt() < 97) return ch.toLowerCase();
+        else return ch.toUpperCase();
     })
     
     return result.join('');
