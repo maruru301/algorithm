cff4dfcec610d319bbb477c6794d70cbbf65193d
윤진 <151013932+maruru301@users.noreply.github.com>
2025-09-29 21:35:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const numStr = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
+    
+    numStr.forEach((str, idx) => {
+        numbers = numbers.replaceAll(str, idx)
+    })
+
+    return Number(numbers);
+}
