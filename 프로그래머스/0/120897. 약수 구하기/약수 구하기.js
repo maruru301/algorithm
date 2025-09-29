a1967526e558b511146af9ab2c87a6bb2761d64e
윤진 <151013932+maruru301@users.noreply.github.com>
2025-09-29 22:27:47 +0900

    [level 0] Title: 약수 구하기, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let result = [];
+    
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) {
+            result.push(i);
+        }
+    }
+    
+    return result;
+}
