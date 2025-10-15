196c5f55bb991ad09ea751968b74056f933719b3
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-15 13:27:11 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,5 +1,7 @@
 function solution(spell, dic) {
     for (let str of dic) {
+        if (str.length !== spell.length) continue; // 단어 길이 다르면 패스
+        
         const isMatch = spell.every(s => str.includes(s));
         if (isMatch) return 1;
     }
