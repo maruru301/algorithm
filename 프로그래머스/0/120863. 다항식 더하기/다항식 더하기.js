faa59b8aa5e10b3a38832c8ab4a7e79f9e0d7d88
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-15 11:40:31 +0900

    [level 0] Title: 다항식 더하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,18 @@
+function solution(polynomial) {
+    let result = '';
+    let xSum = 0;
+    let numSum = 0;
+
+    polynomial.split(' + ').forEach(el => {
+        if (el.includes('x')) {
+            const xCount = el === 'x' ? 1 : Number(el.replace('x', ''));
+            xSum += xCount;
+        } else {
+            numSum += Number(el);
+        }
+    })
+    
+    if (xSum && numSum) return `${xSum === 1 ? '' : xSum}x + ${numSum}`;
+    if (xSum) return `${xSum === 1 ? '' : xSum}x`;
+    return `${numSum}`;
+}
