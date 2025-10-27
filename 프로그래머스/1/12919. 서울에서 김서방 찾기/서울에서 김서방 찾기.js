608b95a5c9d32cc4d60d9b4606c218caea252ff6
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-27 16:21:04 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,9 @@
+function solution(seoul) {
+    var answer = '';
+    
+    seoul.forEach((name, index) => {
+        if (name === 'Kim') answer = `김서방은 ${index}에 있다`
+    });
+    
+    return answer
+}
