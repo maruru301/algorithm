fbda994086660aa7cac393f878088a32c45d4676
윤진 <151013932+maruru301@users.noreply.github.com>
2025-11-05 16:24:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.62 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,14 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
+    const map = new Map();
+
+    // 참가자 카운트
+    participant.forEach(p => map.set(p, (map.get(p) || 0) + 1));
+
+    // 완주자 1씩 감소
+    completion.forEach(c => map.set(c, map.get(c) - 1));
     
-    for (let i = 0; i < completion.length; i++) {
-    if (participant[i] !== completion[i]) {
-        return participant[i];
-        }
+    // 남아있는 카운트가 1 이상인 사람 반환
+    for (const [key, value] of map) {
+        if (value > 0) return key;
     }
-    
-    return participant[participant.length - 1]
 }
