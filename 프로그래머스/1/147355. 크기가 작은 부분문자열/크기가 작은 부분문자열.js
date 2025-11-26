7660a26974e18c8250c467c2b75986154ee40b78
윤진 <151013932+maruru301@users.noreply.github.com>
2025-11-26 22:20:00 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.40 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,16 @@
+function solution(t, p) {
+    let count = 0;
+    
+    const len = t.length - (p.length - 1); // 부분 문자열 개수
+    
+    for (let i = 0; i < len; i++) {
+        const subStr = t.substr(i, p.length);
+        
+        if (Number(subStr) <= Number(p)) {
+            count++;
+        }
+    }
+    
+    
+    return count;
+}
