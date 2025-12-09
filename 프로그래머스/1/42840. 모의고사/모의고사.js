1034e932d3b23cdffe6dcaf6e04659dfeb70a34e
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-09 16:34:18 +0900

    [level 1] Title: 모의고사, Time: 4.13 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,25 +1,23 @@
 function solution(answers) {
-    let p1 = [1, 2, 3, 4, 5]
-    let p2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    let p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-  
+    const result = [];
+    const p1 = [1, 2, 3, 4, 5];
+    const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     let count1 = 0;
     let count2 = 0;
     let count3 = 0;
     
     for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === p1[i % p1.length]) count1++
-        if (answers[i] === p2[i % p2.length]) count2++
-        if (answers[i] === p3[i % p3.length]) count3++
+        if (answers[i] === p1[i % p1.length]) count1++;
+        if (answers[i] === p2[i % p2.length]) count2++;
+        if (answers[i] === p3[i % p3.length]) count3++;
     }
     
-    let arr = [count1, count2, count3];
-    const max = Math.max(...arr);
+    const max = Math.max(count1, count2, count3);
     
-    let result = [];
-    for (let i = 0; i < 3; i++) {
-        if (arr[i] === max) result.push(i + 1);
-    }
+    if (max === count1) result.push(1);
+    if (max === count2) result.push(2);
+    if (max === count3) result.push(3);
     
     return result;
 }
