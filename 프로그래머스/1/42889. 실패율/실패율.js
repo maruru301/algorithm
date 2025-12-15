00d88b294fb53c99d185e941a1a6fa98abb29e07
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-15 13:51:16 +0900

    [level 1] Title: 실패율, Time: 29.68 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,24 +1,28 @@
 function solution(N, stages) {
-    const counts = Array(N + 1).fill(0);  // 1~N 스테이지 카운트
-
-    // 각 스테이지에 멈춘 사람 수 카운트
-    for (const stage of stages) {
-        if (stage <= N) counts[stage]++;
-    }
-
+    let failRates = [];
     let players = stages.length;
-    const failRates = [];
-
-    // 스테이지별 실패율 계산
+    const stageMap = {}
+    
+    // 각 스테이지 번호 초기화
     for (let i = 1; i <= N; i++) {
-        const fail = counts[i] / players;
-        failRates.push({ stage: i, rate: fail });
-
-        players -= counts[i];  // 다음 스테이지로 갈 사람 감소
+        stageMap[i] = 0;
     }
-
-    // 실패율 내림차순 정렬 후 스테이지만 반환
+    
+    // 각 스테이지에 있는 유저 카운트
+    stages.forEach(stage => {
+        if (stage <= N) stageMap[stage]++;
+    })
+    
+    // 실패율 계산
+    for (let i = 1; i <= N; i++) {
+        const fail = stageMap[i] / players;
+        
+        failRates.push({ stage: i, rate: fail });
+        
+        players -= stageMap[i];
+    }
+    
     return failRates
-        .sort((a, b) => b.rate - a.rate)
-        .map(v => v.stage);
+        .sort((a, b) => b.rate - a.rate) // 내림차순
+        .map(el => el.stage); // 스테이지만 반환
 }
